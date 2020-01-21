8b95164f568a51b747dd473a233c80f88d7dfa8b
yogaap24 <yogaspenma48@gmail.com>
2020-01-21 15:47:23 +0700

    New

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "yogaapazure.database.windows.net";
+    $user = "yogaap";
+    $pass = "Inikatasandi24";
+    $db = "yogaazure";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
